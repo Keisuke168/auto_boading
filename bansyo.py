35c9fbeb80807c46fd0ddd96e8cce59b4352132f
Your Name <you@example.com>
2019-10-09 07:20:34 +0000

    bug fix

--- bansyo.py
+++ bansyo.py
@@ -18,20 +18,15 @@ class BansyoCam():
 		self.img = self.img.convert("L")
 		self.img = ImageOps.invert(self.img)
 		self.miniimg = self.img.copy()
-		self.miniimg = 
-self.miniimg.resize((round(self.img.width*0.2),round(self.img.height*0.2)))
+		self.miniimg = self.miniimg.resize((round(self.img.width*0.2),round(self.img.height*0.2)))
 		
 	def monolize(self, i):
-		self.imgmono = self.img.point(lambda x: 0 if x < i 
-else x)
-		self.imgmono = self.imgmono.point(lambda x: 255 if 
-x >= i else x)
+		self.imgmono = self.img.point(lambda x: 0 if x < i else x)
+		self.imgmono = self.imgmono.point(lambda x: 255 if x >= i else x)
 		
 	def Fastmonolize(self,i):
-		self.miniimgmono = self.miniimg.point(lambda x: 0 
-if x < i else x)
-		self.miniimgmono = self.miniimgmono.point(lambda 
-x: 255 if x >= i else x)
+		self.miniimgmono = self.miniimg.point(lambda x: 0 if x < i else x)
+		self.miniimgmono = self.miniimgmono.point(lambda x: 255 if x >= i else x)
 		
 	def transpalent(self):
 		temp = self.imgmono.copy()
@@ -53,8 +48,7 @@ x: 255 if x >= i else x)
 	def pil2ui(self, imgIn):
 		with io.BytesIO() as bIO:
 			imgIn.save(bIO, 'PNG')
-			imgOut = 
-ui.Image.from_data(bIO.getvalue())
+			imgOut = ui.Image.from_data(bIO.getvalue())
 		del bIO
 		return imgOut
 		
@@ -79,17 +73,14 @@ class myview(ui.View):
 		
 		self.imageView = ui.ImageView()
 		self.imageView.width = self.width
-		self.imageView.center = (self.width * 0.5, 
-self.height * 0.5)
+		self.imageView.center = (self.width * 0.5, self.height * 0.5)
 		self.imageView.flex = 'WHB'
 		self.imageView.image = self.bbimg.getImg()
-		self.imageView.content_mode = 
-ui.CONTENT_SCALE_ASPECT_FILL
+		self.imageView.content_mode = ui.CONTENT_SCALE_ASPECT_FILL
 		
 		self.sliderView = ui.Slider()
 		self.sliderView.width = self.width*0.8
-		self.sliderView.center = (self.width * 0.5, 
-self.height * 0.8)
+		self.sliderView.center = (self.width * 0.5, self.height * 0.8)
 		self.sliderView.flex = 'WT'
 		self.sliderView.value = 0.5
 		#self.sliderView.continuous = False
@@ -98,48 +89,37 @@ self.height * 0.8)
 		self.button = ui.Button(title='Complete')
 		self.button.flex = 'WT'
 		self.button.width = self.width*0.2
-		self.button.center = (self.width * 0.5, 
-self.height * 0.64)
+		self.button.center = (self.width * 0.5, self.height * 0.64)
 		self.button.action = self.button_tapped
 		
 		self.textfield1 = ui.TextField()
 		self.textfield1.width = self.width* 0.1
 		self.textfield1.height = 36
-		self.textfield1.center = 
-(self.width*0.1,self.height*0.70)
+		self.textfield1.center = (self.width*0.1,self.height*0.70)
 		self.textfield1.text = str(self.min)
-		self.textfield1.keyboard_type = 
-ui.KEYBOARD_NUMBERS
+		self.textfield1.keyboard_type = ui.KEYBOARD_NUMBERS
 		self.textfield1.action = self.textfield1_edit
 		
 		self.textfield2 = ui.TextField()
 		self.textfield2.width = self.width* 0.1
 		self.textfield2.height = 36
-		self.textfield2.center = 
-(self.width*0.9,self.height*0.70)
+		self.textfield2.center = (self.width*0.9,self.height*0.70)
 		self.textfield2.text = str(self.max)
-		self.textfield2.keyboard_type = 
-ui.KEYBOARD_NUMBERS
+		self.textfield2.keyboard_type = ui.KEYBOARD_NUMBERS
 		self.textfield2.action = self.textfield2_edit
 		
 		self.label = ui.Label()
 		self.label.width = self.width*0.2
 		self.label.height=36
-		self.label.center=(self.width*0.57, 
-self.height*0.7)
+		self.label.center=(self.width*0.57, self.height*0.7)
 		self.label.text = str(self.i)
 		
 		if iPad == True:
-			self.imageView.center = (self.width * 0.5, 
-self.height * 0.42)
-			self.sliderView.center = (self.width * 
-0.5, self.height * 0.75)
-			self.button.center = (self.width * 0.5, 
-self.height * 0.87)
-			self.textfield1.center = 
-(self.width*0.1,self.height*0.70)
-			self.textfield2.center = 
-(self.width*0.9,self.height*0.70)
+			self.imageView.center = (self.width * 0.5, self.height * 0.42)
+			self.sliderView.center = (self.width * 0.5, self.height * 0.75)
+			self.button.center = (self.width * 0.5, self.height * 0.87)
+			self.textfield1.center = (self.width*0.1,self.height*0.70)
+			self.textfield2.center = (self.width*0.9,self.height*0.70)
 		
 		self.add_subview(self.imageView)
 		self.add_subview(self.sliderView)
@@ -149,8 +129,7 @@ self.height * 0.87)
 		self.add_subview(self.label)
 		
 	def sliderAction(self, sender):
-		self.i = sender.value * (self.max - self.min) + 
-self.min
+		self.i = sender.value * (self.max - self.min) + self.min
 		self.bbimg.Fastmonolize(self.i)
 		#print(self.i)
 		self.draw()
@@ -161,7 +140,7 @@ self.min
 		self.bbimg.copy()
 		ok = ui.Button(title='OK')
 		
-dialogs.alert('Copied!','','OK',hide_cancel_button=True)
+		dialogs.alert('Copied!','','OK',hide_cancel_button=True)
 		self.close()
 	
 	def change_value(self):
@@ -170,8 +149,7 @@ dialogs.alert('Copied!','','OK',hide_cancel_button=True)
 		elif self.i <= self.min:
 			self.sliderView.value = 0.0
 		else:
-			self.sliderView.value = (self.i - 
-self.min)/(self.max - self.min)
+			self.sliderView.value = (self.i - self.min)/(self.max - self.min)
 			
 		
 	def textfield1_edit(self,sender):
